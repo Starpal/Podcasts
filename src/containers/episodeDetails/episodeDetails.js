6339bdb5d8da71dbe5a5c55f2c70d835569370db
Paola Vezzaro <vezzaro@strands.com>
2023-02-13 20:35:34 +0100

    configured webpack.prod and webpack.build

--- src/containers/episodeDetails/episodeDetails.js
+++ src/containers/episodeDetails/episodeDetails.js
@@ -3,7 +3,6 @@ import { Link, useLocation } from 'react-router-dom';
 import "./episodeDetails.scss";
 import parse from 'html-react-parser';
 
-
 const episodeDetails = () => {
 
 	const location = useLocation();
--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,10 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	output: {
 		path: path.join(__dirname, "/dist"),
 		filename: "bundle.js",
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "src/index.html",
-		}),
-	],
 	devServer: {
 		port: 3000,
 	},
@@ -21,11 +15,10 @@ module.exports = {
 				exclude: /node_modules/,
 				use: {
 					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-react", "@babel/preset-env"],
+					},
 				},
-			},
-			{
-				test: /\.(sa|sc|c)ss$/,
-				use: ["style-loader", "css-loader", "sass-loader"],
 			},
 			{
 				test: /\.(png|woff|woff2|eot|ttf|svg)$/,
